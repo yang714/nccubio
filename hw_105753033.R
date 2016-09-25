6d4bfaad2ace61f64ddfeec6ddecdf1262d3f6df
yang714 <ceerr714@gmail.com>
2016-09-25 17:14:44 +0800

    118

---
+++ hw_105753033.R
@@ -0,0 +1,26 @@
+# read PAM1 from data
+pam1<-read.table("pam1.txt")
+
+# check PAM1 data
+dim(pam1)
+str(pam1)
+#class(pam1)
+
+# construct PAM250 from PAM1
+pam2<-as.matrix(pam1)
+#pam2=pam2/10000
+#pam2<-round(pam2, digit = 3)
+pam250<-pam2
+#class(pam2)
+for(ggc in c(1:249))
+{
+  pam250 <- pam250%*%pam2
+  #Reduce("%*%", pam2, accumulate = TRUE)
+  #pam2<-pam2+pam2
+}
+data<-pam250
+
+#
+# output PAM250 as a file
+write.table(data,file="pam250.txt")
+
